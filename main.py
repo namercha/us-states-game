a3a9cf9bc7c023c6f88b9a4f6024edfddddb9657
nabil <merchant.nabil@gmail.com>
2021-11-04 21:20:45 -0500

    Initial setup commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+
+
+
+
+
+turtle.mainloop()
