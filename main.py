0d8e5979c7c5fcf9969e3baf2d9b906ffc4f07b1
nabil <merchant.nabil@gmail.com>
2021-11-04 21:44:32 -0500

    Guessing game for US states game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,10 +7,35 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    # print(answer_state)
+    if answer_state == "Exit":
+        missing_states = []
+        # Find all the states that haven't been guessed and add them to a csv
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    # If answer_state is one of the 50 states in 50_states.csv
+    if answer_state in all_states:
+        # if the input was correct,
+        # Create a turtle to write in the x and y coordinates on the image
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 
+# states_to_learn.csv
+# Generate CSV for states that have not been guessed
 
-
-
-turtle.mainloop()
