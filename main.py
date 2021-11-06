b600187424fdf4b19a286617ee75d49667c8b85b
nabil <merchant.nabil@gmail.com>
2021-11-05 20:41:56 -0500

    adding a list comprehension example

--- main.py
+++ main.py
@@ -16,11 +16,13 @@ while len(guessed_states) < 50:
                                     prompt="What's another state's name?").title()
     # print(answer_state)
     if answer_state == "Exit":
-        missing_states = []
+        # missing_states = []
         # Find all the states that haven't been guessed and add them to a csv
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
+
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
